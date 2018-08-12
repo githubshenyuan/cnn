916e5c7c5699dc04b4defd45ab961de11a80a93c
githubshenyuan <35088383+githubshenyuan@users.noreply.github.com>
2018-08-12 21:54:01 +0800

    cnn

--- begin.py
+++ begin.py
@@ -1,5 +1,25 @@
-import os,numpy
+from keras.datasets import mnist, imdb
+from keras import models
+from keras import layers
+from keras.utils import to_categorical
+
+
+def get_mnist():
+    (train_images, train_labels), (test_images, test_labels) = mnist.load_data("./mnist.npz")
+    network = models.Sequential()
+    network.add(layers.Dense(512, activation='relu', input_shape=(28 * 28,)))
+    network.add(layers.Dense(10, activation='softmax'))
+    network.compile(optimizer='rmsprop',
+                    loss='categorical_crossentropy',
+                    metrics=['accuracy'])
+    train_images = train_images.reshape((60000, 28 * 28))
+    train_images = train_images.astype('float32') / 255
+    test_images = test_images.reshape((10000, 28 * 28))
+    test_images = test_images.astype('float32') / 255
+    train_labels = to_categorical(train_labels)
+    test_labels = to_categorical(test_labels)
+    network.fit(train_images, train_labels, epochs=5, batch_size=128)
+
 
 if __name__ == '__main__':
-    a = numpy.zeros((5,3,4))
-    print("呵呵")
+    (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
